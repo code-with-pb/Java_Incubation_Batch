9040d7268a3db7c6593aab80970b7ac0e698e2d3
code-with-pb <98020094+code-with-pb@users.noreply.github.com>
2024-02-02 13:33:10 +0530

    Update Fact.java

--- Feb_1/Fact.java
+++ Feb_1/Fact.java
@@ -1,9 +1,9 @@
 class Fact{
     public static void main(String args[]){
         int n1 = Integer.parseInt(args[0]);
-        int i = 1, s = 0;
+        int i = 1, s = 1;
         while(i<=n1){
-            s += i;
+            s *= i;
             i++;
         }
         System.out.println("Factorial is: "+s);
